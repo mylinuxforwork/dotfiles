c378222c83463260ab4dfd010fb47e380b9ce414
Stephan Raabe <mail@stephan-raabe.de>
2023-02-20 15:57:18 +0100

    Update vm launch

--- scripts/launchvm.sh
+++ scripts/launchvm.sh
@@ -4,6 +4,6 @@ virsh --connect qemu:///system start win11
 echo "Waiting 20 sec for Windows 11 startup..."
 sleep 20
 echo "Starting xfreerdp now..."
-xfreerdp -grab-keyboard /t:Windows11 /v:192.168.122.42 /size:100% /d: /p:sancho /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
+xfreerdp -grab-keyboard /t:"Windows 11" /v:192.168.122.42 /size:100% /d: /p:sancho /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
 
 exit
