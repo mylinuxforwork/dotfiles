bbe6e8dbbcc962bae036a80f2c5df25dd2408f5c
Stephan Raabe <mail@stephan-raabe.de>
2023-07-31 13:34:19 +0200

    Updates

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#   ___ _____ ___ _     _____   ____  _             _    
+#  / _ \_   _|_ _| |   | ____| / ___|| |_ __ _ _ __| |_  
+# | | | || |  | || |   |  _|   \___ \| __/ _` | '__| __| 
+# | |_| || |  | || |___| |___   ___) | || (_| | |  | |_  
+#  \__\_\|_| |___|_____|_____| |____/ \__\__,_|_|   \__| 
+#                                                        
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
 
 xrandr --rate 120
 setxkbmap de
@@ -6,5 +15,5 @@ picom &
 xfce4-power-manager &
 dunst &
 ~/dotfiles/polybar/launch.sh &
-~/dotfiles/scripts/updatewal.sh
-spotifyd
+~/dotfiles/scripts/updatewal.sh &
+virsh --connect qemu:///system start win11
