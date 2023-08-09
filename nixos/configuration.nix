f3e8013669385bad6ed500c97d68a3575170b446
Stephan Raabe <mail@stephan-raabe.de>
2023-08-09 14:53:26 +0000

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,7 +74,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     qtile
     alacritty
