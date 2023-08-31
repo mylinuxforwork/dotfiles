a3a745fee99c5de693a4ce18944a54e4fab05694
Stephan Raabe <mail@stephan-raabe.de>
2023-08-31 11:43:42 +0200

    Updates

---
+++ scripts/updates.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#  _   _           _       _             
+# | | | |_ __   __| | __ _| |_ ___  ___  
+# | | | | '_ \ / _` |/ _` | __/ _ \/ __| 
+# | |_| | |_) | (_| | (_| | ||  __/\__ \ 
+#  \___/| .__/ \__,_|\__,_|\__\___||___/ 
+#       |_|                              
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+    updates_aur=0
+fi
+
+updates=$(("$updates_arch" + "$updates_aur"))
+
+if [ "$updates" -gt 0 ]; then
+    echo " $updates"
+else
+    echo "0"
+fi
