79d955a4bafb5c0a4ad8c142450761f22ad9aa86
Stephan Raabe <mail@stephan-raabe.de>
2023-09-07 08:35:26 +0200

    Updates

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -9,11 +9,30 @@
 # by Stephan Raabe (2023) 
 # ----------------------------------------------------- 
 
+# My screen resolution
 xrandr --rate 120
+
+# For Virtual Machine 
+# xrandr --outout Virtual-1 --mode 1920x1080
+
+# Set keyboard mapping
 setxkbmap de
+# setxkbmap en
+
+# Load picom
 picom &
+
+# Load power manager
 xfce4-power-manager &
+
+# Load notification service
 dunst &
+
+# Launch polybar
 ~/dotfiles/polybar/launch.sh &
+
+# Setup Wallpaper and update colors
 ~/dotfiles/scripts/updatewal.sh &
+
+# Load Windows 11 VM
 # virsh --connect qemu:///system start win11
