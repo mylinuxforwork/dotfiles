6969b2fec0450c05c8e1f7bbf0b1d14c180a67b0
Stephan Raabe <mail@stephan-raabe.de>
2023-09-21 14:04:51 +0200

    Remove first option

--- scripts/grim.sh
+++ scripts/grim.sh
@@ -12,20 +12,14 @@
 DIR="$HOME/Pictures/screenshots/"
 NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
 
-# option1="Selected window (delay 3 sec)"
 option2="Selected area"
 option3="Fullscreen (delay 3 sec)"
 
-# options="$option1\n$option2\n$option3"
 options="$option2\n$option3"
 
 choice=$(echo -e "$options" | rofi -dmenu -i -no-show-icons -l 4 -width 30 -p "Take Screenshot")
 
 case $choice in
-    $option1)
-        scrot $DIR$NAME -d 3 -e 'xclip -selection clipboard -t image/png -i $f' -c -z -u
-        notify-send "Screenshot created" "Mode: Selected window"
-    ;;
     $option2)
         grim -g "$(slurp)" $(xdg-user-dir PICTURES)/screenshots/$(date +'%s_grim.png')
         notify-send "Screenshot created" "Mode: Selected area"
