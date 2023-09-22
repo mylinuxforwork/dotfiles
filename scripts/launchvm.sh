0eb669548bc134b16c9d3ca61f1e579884cf1c42
Stephan Raabe <mail@stephan-raabe.de>
2023-09-22 17:45:38 +0200

    Updates

--- scripts/launchvm.sh
+++ scripts/launchvm.sh
@@ -9,11 +9,20 @@
 # by Stephan Raabe (2023) 
 # ----------------------------------------------------- 
 
+if [ -f ~/private/win11-credentials.sh ]; then
+    echo "Credential file exists. Using the file."
+    source ~/private/win11-credentials.sh
+else
+    win11user="USER"
+    win11pass="PASS"
+fi
+
 tmp=$(virsh --connect qemu:///system list | grep " win11 " | awk '{ print $3}')
+
 if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
 then
     virsh --connect qemu:///system start win11
-    echo "Virtual Machine win11 is starting..."
+    echo "Virtual Machine win11 is starting... Waiting 45s for booting up."
     notify-send "Virtual Machine win11 is starting..." "Waiting 45s for booting up."
     sleep 45
 else
@@ -21,4 +30,4 @@ else
     echo "Starting xfreerdp now..."
 fi
 
-xfreerdp -grab-keyboard /v:192.168.122.44 /size:100% /cert-ignore /u:raabe /p:SECRET /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
+xfreerdp -grab-keyboard /v:192.168.122.44 /size:100% /cert-ignore /u:$win11user /p:$win11pass /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
