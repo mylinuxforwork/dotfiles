09a64254fc99aef169c68ee25393417d92689f65
Stephan Raabe <mail@stephan-raabe.de>
2023-10-04 17:39:24 +0000

    Update lockscreentime.sh idle times doubled.

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -2,7 +2,7 @@
 
 if [ -f "/usr/bin/swayidle" ]; then
     echo "swayidle is installed."
-    swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+    swayidle -w timeout 600 'swaylock -f' timeout 660 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
 else
     echo "swayidle not installed."
 fi;
