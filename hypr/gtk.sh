fe350bfe16895d9b37fe24a05ea395312380f7ed
Stephan Raabe <mail@stephan-raabe.de>
2023-10-04 14:18:31 +0200

    Update cursor to Bibata Modern Ice

--- hypr/gtk.sh
+++ hypr/gtk.sh
@@ -4,7 +4,7 @@ gnome_schema="org.gnome.desktop.interface"
 
 gsettings set "$gnome_schema" gtk-theme "Breeze-Dark"
 gsettings set "$gnome_schema" icon-theme "kora"
-gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Classic"
+gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Ice"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
