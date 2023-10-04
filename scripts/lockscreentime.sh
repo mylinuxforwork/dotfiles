ea6f98fd4f14e51d64afbec59d428d5c3f604657
Stephan Raabe <mail@stephan-raabe.de>
2023-10-04 14:18:59 +0200

    Update swayidle times

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -1,8 +1,20 @@
 #/bin/sh
+#  ___    _ _      _   _                 
+# |_ _|__| | | ___| |_(_)_ __ ___   ___  
+#  | |/ _` | |/ _ \ __| | '_ ` _ \ / _ \ 
+#  | | (_| | |  __/ |_| | | | | | |  __/ 
+# |___\__,_|_|\___|\__|_|_| |_| |_|\___| 
+#                                        
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+timeswaylock=600
+timeoff=660
 
 if [ -f "/usr/bin/swayidle" ]; then
     echo "swayidle is installed."
-    swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+    swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
 else
     echo "swayidle not installed."
 fi;
