db790f413f42051eddeb9a1f2472c9701d1aea70
Stephan Raabe <mail@stephan-raabe.de>
2023-10-17 17:06:58 +0200

    Updates

--- scripts/templates.sh
+++ scripts/templates.sh
@@ -12,7 +12,6 @@
 # Select text file
 selected=$(ls -1 ~/private/templates | rofi -dmenu -p "Select the template")
 
-
 if [ "$selected" ]; then
     # Add content to clipboard
     xclip -sel clip ~/private/templates/$selected
