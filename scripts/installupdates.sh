2b1a2d7ffae47b228159ed2db03d086c50fe81d9
Stephan Raabe <mail@stephan-raabe.de>
2023-11-12 19:57:18 +0100

    Bugfix library

--- scripts/installupdates.sh
+++ scripts/installupdates.sh
@@ -11,8 +11,28 @@
 # ----------------------------------------------------- 
 
 sleep 1
-source ~/dotfiles/.install/library.sh
 clear
+
+cat <<"EOF"
+ _   _           _       _            
+| | | |_ __   __| | __ _| |_ ___  ___ 
+| | | | '_ \ / _` |/ _` | __/ _ \/ __|
+| |_| | |_) | (_| | (_| | ||  __/\__ \
+ \___/| .__/ \__,_|\__,_|\__\___||___/
+      |_|                             
+
+EOF
+
+_isInstalledYay() {
+    package="$1";
+    check="$(yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
 
 # ------------------------------------------------------
 # Confirm Start
