ea1ee7cca33acc9110b81bc888cce27d8f3ba161
Stephan Raabe <mail@stephan-raabe.de>
2023-11-19 11:28:33 +0100

    Update to python-pywal

--- .install/pywal.sh
+++ .install/pywal.sh
@@ -13,9 +13,10 @@ cat <<"EOF"
 EOF
 echo -e "${NONE}"
 if [ -f /usr/bin/wal ]; then
-    echo "pywal already installed."
+    echo "pywal already installed. Trying to force the installation with python-pywal"
+    yay -S --noconfirm python-pywal --ask 4
 else
-    yay --noconfirm -S pywal
+   yay -S --noconfirm python-pywal
     echo "Pywal installed."
 fi
 echo ""
