140dc369ed54f3df7ba2d025ca4be6862be40088
Stephan Raabe <mail@stephan-raabe.de>
2023-12-07 10:16:50 +0100

    Add qemu-guest-agent installation

--- .install/vm.sh
+++ .install/vm.sh
@@ -10,8 +10,12 @@ cat <<"EOF"
                                        
 EOF
 echo -e "${NONE}"
-    if gum confirm "Are you running this script in a KVM virtual machine?" ;then
+    echo "The script has detected that you run the installation in a KVM virtual machine."
+    if gum confirm "Do you want to install the KVM environment variables?" ;then
         echo "source = ~/dotfiles/hypr/conf/environments/kvm.conf" >  ~/dotfiles-versions/$version/hypr/conf/environment.conf
         echo "Environment set to KVM."
+    fi
+    if gum confirm "Do you want to install the QEMU guest agent?" ;then
+        _installPackagesPacman "qemu-guest-agent";
     fi
 fi
