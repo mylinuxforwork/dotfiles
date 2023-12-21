91c289bb2e0795610bb54b2606505d10e7e4a0f2
Stephan Raabe <mail@stephan-raabe.de>
2023-12-21 13:14:11 +0100

    Updates

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -41,10 +41,10 @@ case $1 in
     # Select wallpaper with rofi
     "select")
 
-        selected=$( find "$HOME/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort | while read rfile
+        selected=$( find "$HOME/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
         do
             echo -en "$rfile\x00icon\x1f$HOME/wallpaper/${rfile}\n"
-        done | rofi -dmenu -replace -l 6 -config ~/dotfiles/rofi/config-wallpaper.rasi)
+        done | rofi -dmenu -replace -config ~/dotfiles/rofi/config-wallpaper.rasi)
         if [ ! "$selected" ]; then
             echo "No wallpaper selected"
             exit
