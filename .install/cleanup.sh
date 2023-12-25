57c7c377f61e56e722ce6f890ec0469ca6621820
Stephan Raabe <mail@stephan-raabe.de>
2023-12-25 15:06:09 +0100

    Update

--- .install/cleanup.sh
+++ .install/cleanup.sh
@@ -1,7 +1,9 @@
 # ------------------------------------------------------
 # Install tty login and issue
 # ------------------------------------------------------
-
+echo -e "${GREEN}"
+figlet "Cleanup"
+echo -e "${NONE}"
 # Check for ttf-ms-fonts
 if [[ $(_isInstalledPacman "ttf-ms-fonts") == 0 ]]; then
     echo "The script has detected ttf-ms-fonts. This can cause conflicts with icons in Waybar."
@@ -9,3 +11,4 @@ if [[ $(_isInstalledPacman "ttf-ms-fonts") == 0 ]]; then
         sudo pacman --noconfirm -R ttf-ms-fonts
     fi
 fi
+echo "Cleanup done."
