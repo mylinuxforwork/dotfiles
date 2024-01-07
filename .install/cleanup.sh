7fea138ab49521fab95eaed37336b25ff9703f49
Stephan Raabe <mail@stephan-raabe.de>
2024-01-07 09:38:00 +0100

    Update

--- .install/cleanup.sh
+++ .install/cleanup.sh
@@ -13,14 +13,14 @@ if [[ $(_isInstalledPacman "ttf-ms-fonts") == 0 ]]; then
     fi
 fi
 
-# Check for running NetworkManager.service
-# if [[ $(systemctl list-units --all -t service --full --no-legend "NetworkManager.service" | sed 's/^\s*//g' | cut -f1 -d' ') == "NetworkManager.service" ]];then
-#     echo "NetworkManager.service already running."
-# else
-#     sudo systemctl enable NetworkManager.service
-#     sudo systemctl start NetworkManager.service
-#     echo "NetworkManager.service activated successfully."    
-# fi
+# Check for running bluetooth.service
+if [[ $(systemctl list-units --all -t service --full --no-legend "bluetooth.service" | sed 's/^\s*//g' | cut -f1 -d' ') == "bluetooth.service" ]];then
+    echo "bluetooth.service already running."
+else
+    sudo systemctl enable bluetooth.service
+    sudo systemctl start bluetooth.service
+    echo "bluetooth.service activated successfully."    
+fi
 
 # Create default folder structure
 xdg-user-dirs-update
