5a0dc33c2da48151d5423da97bd9e5c5af3554ce
Stephan Raabe <mail@stephan-raabe.de>
2024-01-21 10:24:13 +0100

    Updates

--- .install/hyprland-dotfiles.sh
+++ .install/hyprland-dotfiles.sh
@@ -36,18 +36,19 @@ if [ ! $mode == "dev" ]; then
     if [ -d ~/dotfiles-versions/$version/swappy ]; then
         _installSymLink swappy ~/.config/swappy ~/dotfiles/swappy/ ~/.config
     fi
-    if [ -d ~/dotfiles-versions/$version/gtk ]; then
-        _installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
-    fi
-    if [ -d ~/dotfiles-versions/$version/gtk ]; then
-        _installSymLink .Xresources ~/.Xresources ~/dotfiles/gtk/.Xresources ~/.Xresources
-    fi
-    if [ -d ~/dotfiles-versions/$version/gtk ]; then
-        _installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
-    fi
-    if [ -d ~/dotfiles-versions/$version/gtk ]; then
-        _installSymLink gtk-4.0 ~/.config/gtk-4.0 ~/dotfiles/gtk/gtk-4.0/ ~/.config/            
-    fi
+
+    #if [ -d ~/dotfiles-versions/$version/gtk ]; then
+    #    _installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+    #fi
+    #if [ -d ~/dotfiles-versions/$version/gtk ]; then
+    #    _installSymLink .Xresources ~/.Xresources ~/dotfiles/gtk/.Xresources ~/.Xresources
+    #fi
+    #if [ -d ~/dotfiles-versions/$version/gtk ]; then
+    #    _installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
+    #fi
+    #if [ -d ~/dotfiles-versions/$version/gtk ]; then
+    #    _installSymLink gtk-4.0 ~/.config/gtk-4.0 ~/dotfiles/gtk/gtk-4.0/ ~/.config/            
+    #fi
 else
     echo "Skipped: DEV MODE!"
 fi
--- gtk/gtk.sh
+++ gtk/gtk.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
-gsettings set org.gnome.desktop.interface font-name 'Cantarell 11'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
