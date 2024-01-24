bd0cd2a4163591669758947168c68b4567f94161
Stephan Raabe <mail@stephan-raabe.de>
2024-01-24 11:49:10 +0100

    Update

--- .install/profile.sh
+++ .install/profile.sh
@@ -8,15 +8,9 @@ echo -e "${NONE}"
 echo "SPACE = select/unselect a profile. RETURN = confirm. No selection = CANCEL"
 profile=$(gum choose --no-limit --cursor-prefix "( ) " --selected-prefix "(x) " --unselected-prefix "( ) " "Hyprland" "Qtile")
 
-IFS=' ' read -r -a profile_array <<< "$profile"
-
-echo $profile_array
-
-echo "${#profile_array[@]}"
-
-# if [ -z $profile ] ;then
-#     echo "No profile selected. Installation canceled."
-#     exit
-# else
-#     echo "Profile/s selected: $profile"
-# fi
+if [ -z "${profile}" ] ;then
+    echo "No profile selected. Installation canceled."
+    exit
+else
+    echo "Profile/s selected:" $profile
+fi
