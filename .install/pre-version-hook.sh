793b9f8a231eb65d9c71afc4cd0b2fbb3d790a1e
Paolo Denti <paolo.denti@gmail.com>
2024-01-27 08:33:00 -0800

    version hooks

--- .install/pre-version-hook.sh
+++ .install/pre-version-hook.sh
@@ -1,13 +1,13 @@
 # ------------------------------------------------------
 # Copy dotfiles
 # ------------------------------------------------------
-if [ -f ~/dotfiles-versions/pre-version-hook.sh ]; then
+if [ -f ~/dotfiles-versions/$version/pre-version-hook.sh ]; then
     echo -e "${GREEN}"
     figlet "Pre Version Hook"
     echo -e "${NONE}"
     echo ":: The script has detected a pre-version-hook.sh script."
     if gum confirm "Do you want to run the script now?"; then
-        source ~/dotfiles-versions/pre-version-hook.sh
+        source ~/dotfiles-versions/$version/pre-version-hook.sh
         echo ":: pre-version-hook.sh executed!"
     else
         echo ":: Execution of pre-version-hook.sh skipped."
--- .install/version-hook.sh
+++ .install/version-hook.sh
@@ -1,13 +1,13 @@
 # ------------------------------------------------------
 # Copy dotfiles
 # ------------------------------------------------------
-if [ -f ~/dotfiles-versions/version-hook.sh ]; then
+if [ -f ~/dotfiles-versions/$version/version-hook.sh ]; then
     echo -e "${GREEN}"
     figlet "Version Hook"
     echo -e "${NONE}"
     echo ":: The script has detected a version-hook.sh script."
     if gum confirm "Do you want to run the script now?"; then
-        source ~/dotfiles-versions/version-hook.sh
+        source ~/dotfiles-versions/$version/version-hook.sh
         echo ":: version-hook.sh executed!"
     else
         echo ":: Execution of version-hook.sh skipped."
