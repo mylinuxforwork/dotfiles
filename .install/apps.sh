ebdb1499996b12e2dd0c9d3009f0562d8731325a
Stephan Raabe <mail@stephan-raabe.de>
2024-02-29 17:33:59 +0100

    Updates

--- .install/apps.sh
+++ .install/apps.sh
@@ -8,6 +8,11 @@ echo -e "${GREEN}"
 figlet "ML4W Apps"
 echo -e "${NONE}"
 
+if [ ! -d $HOME/.local/share/applications/ ] ;then
+    mkdir $HOME/.local/share/applications
+    echo ":: $HOME/.local/share/applications created"
+fi
+
 sed -i "s|HOME|${HOME}|g" $HOME/dotfiles/apps/ml4w-welcome.desktop
 cp $HOME/dotfiles/apps/ml4w-welcome.desktop $HOME/.local/share/applications
 echo ":: ML4W Welcome App installed successfully"
