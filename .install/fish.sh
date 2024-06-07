49fd73e67b073781525d2c109b45bb7351938acf
Yingjie Wang <phywyj@gmail.com>
2024-06-07 03:09:21 -0400

    update: add choice between oh my zsh and zinit

--- .install/fish.sh
+++ .install/fish.sh
@@ -1,16 +1,6 @@
 # ------------------------------------------------------
 # install fish
 # ------------------------------------------------------
-packagesPacman=(
-	"fish"
-);
-packagesYay=(
-);
+packagesPacman=("fish");
+packagesYay=();
 source .install/install-packages.sh
-
-if [ -f ~/dotfiles-versions/$version/.zshrc ]; then
-    _installSymLink zshrc ~/.zshrc ~/dotfiles/.zshrc ~
-fi
-if [ -f ~/dotfiles-versions/$version/.p10k.zsh ]; then
-    _installSymLink p10k ~/.p10k.zsh ~/dotfiles/.p10k.zsh ~
-fi
--- .install/zsh.sh
+++ .install/zsh.sh
@@ -1,10 +1,19 @@
 # ------------------------------------------------------
 # install zsh and zinit
 # ------------------------------------------------------
-packagesPacman=(
-	"zsh"
-);
-packagesYay=(
-	"zinit"
-);
-source .install/install-packages.sh
+packagesPacman=("zsh");
+
+echo "Please select the zsh plugin manager:"
+zsh_manager=$(gum choose --limit=1 --cursor-prefix "( ) " --selected-prefix "(x) " --unselected-prefix "( ) " "oh my zsh" "zinit")
+if [ ${zsh_manager}=="oh my zsh" ]; then
+    source .install/install-packages.sh
+    RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+	_installSymLink zshrc ~/.zshrc ~/dotfiles/.zshrc_ohmyzsh ~
+else
+    packagesYay=("zinit");
+	source .install/install-packages.sh
+    _installSymLink zshrc ~/.zshrc ~/dotfiles/.zshrc_zinit ~
+fi
+_installSymLink p10k ~/.p10k.zsh ~/dotfiles/.p10k.zsh ~
