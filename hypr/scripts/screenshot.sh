d6fce7ecd39514c4d3e3ccc4d54ba75acfa68ba5
Stephan Raabe <mail@stephan-raabe.de>
2024-06-12 13:00:40 +0200

    Updates

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -14,10 +14,11 @@ NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
 
 option2="Selected area"
 option3="Fullscreen (delay 3 sec)"
+option4="Current display (delay 3 sec)"
 
-options="$option2\n$option3"
+options="$option2\n$option3\n$option4"
 
-choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Take Screenshot")
+choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-screenshot.rasi -i -no-show-icons -l 3 -width 30 -p "Take Screenshot")
 
 case $choice in
     $option2)
@@ -29,6 +30,14 @@ case $choice in
     $option3)
         sleep 3
         grim "$DIR$NAME" 
+        xclip -selection clipboard -t image/png -i "$DIR$NAME"
+        notify-send "Screenshot created and copied to clipboard" "Mode: Fullscreen"
+        swappy -f "$DIR$NAME"
+    ;;
+    $option4)
+        sleep 3
+        monitor="$(hyprctl monitors | awk '/Monitor/{monitor=$2} /focused: yes/{print monitor; exit}')"
+        grim -o "$monitor" "$DIR$NAME"
         xclip -selection clipboard -t image/png -i "$DIR$NAME"
         notify-send "Screenshot created and copied to clipboard" "Mode: Fullscreen"
         swappy -f "$DIR$NAME"
