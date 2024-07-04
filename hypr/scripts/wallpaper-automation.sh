c2eb6ed41838245b0f18df91d59f4a190f57036e
Stephan Raabe <mail@stephan-raabe.de>
2024-07-04 15:28:49 +0200

    Updates

---
+++ hypr/scripts/wallpaper-automation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sec=$(cat ~/dotfiles/.settings/wallpaper-automation.sh)
+_setWallpaperRandomly() {
+    waypaper --random
+    echo ":: Next wallpaper in 60 seconds..."
+    sleep $sec
+    _setWallpaperRandomly
+}
+
+if [ ! -f ~/.cache/ml4w-wallpaper-automation ] ;then
+    touch ~/.cache/ml4w-wallpaper-automation
+    echo ":: Start wallpaper automation script"
+    notify-send "Wallpaper automation process started" "Wallpaper will be changed every $sec seconds."
+    _setWallpaperRandomly
+else
+    rm ~/.cache/ml4w-wallpaper-automation
+    notify-send "Wallpaper automation process stopped."
+    echo ":: Wallpaper automation script process $wp stopped"
+    wp=$(pgrep -f wallpaper-automation.sh)
+    kill -KILL $wp
+fi
