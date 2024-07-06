b771743a25a6eac62295a63f5055ecb5156d4aaf
Stephan Raabe <mail@stephan-raabe.de>
2024-07-06 21:35:51 +0200

    Updates

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -26,22 +26,11 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+if ! updates_aur=$(yay -Qu --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
-
-# ----------------------------------------------------- 
-# Testing
-# ----------------------------------------------------- 
-
-# Overwrite updates with numbers for testing
-# updates=100
-
-# test JSON output
-# printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "red"}'
-# exit
 
 # ----------------------------------------------------- 
 # Output in JSON format for Waybar Module custom-updates
