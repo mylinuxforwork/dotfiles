b10f1d74c131b5ddcb083cd5b00d1e3ddaf3c5a8
Stephan Raabe <mail@stephan-raabe.de>
2024-07-24 11:24:13 +0200

    Add flatpak to the installer

---
+++ .install/flatpak.sh
@@ -0,0 +1,21 @@
+# ------------------------------------------------------
+# Flatpak
+# ------------------------------------------------------
+if [[ $(_isInstalledPacman "flatpak") == 0 ]]; then
+    echo ":: Flatpak already installed"
+else
+    echo -e "${GREEN}"
+    figlet "Flatpak"
+    echo -e "${NONE}"
+    echo
+    echo "Flatpak is a framework for distributing desktop applications across various Linux distributions." 
+    echo "Flatpak applications: these are the applications the user installs via the flatpak command"
+    if gum confirm "Do you want to install flatpak?"; then
+        _installPackagesPacman "qemu-guest-agent";
+    elif [ $? -eq 130 ]; then
+        echo ":: Installation canceled."
+        exit 130
+    else
+        echo ":: Installation of flatpak skipped."
+    fi
+fi
