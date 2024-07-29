e874235578c4f302b9f23c32815e1cd54c6565f8
Stephan Raabe <mail@stephan-raabe.de>
2024-07-29 17:17:53 +0200

    Updates

--- dotfiles/hypr/scripts/wallpaper.sh
+++ dotfiles/hypr/scripts/wallpaper.sh
@@ -26,13 +26,13 @@ fi
 # ----------------------------------------------------- 
 
 force_generate=0
-generated_versions="~/.cache/ml4w-wallpaper-generated"
-cache_file="~/.cache/current_wallpaper"
-blurred_wallpaper="~/.cache/blurred_wallpaper.png"
-square_wallpaper="~/.cache/square_wallpaper.png"
-rasi_file="~/.cache/current_wallpaper.rasi"
-blur_file="~/dotfiles/.settings/blur.sh"
-default_wallpaper="~/wallpaper/default.jpg"
+generated_versions="$HOME/.cache/ml4w-wallpaper-generated"
+cache_file="$HOME/.cache/current_wallpaper"
+blurred_wallpaper="$HOME/.cache/blurred_wallpaper.png"
+square_wallpaper="$HOME/.cache/square_wallpaper.png"
+rasi_file="$HOME/.cache/current_wallpaper.rasi"
+blur_file="$HOME/dotfiles/.settings/blur.sh"
+default_wallpaper="$HOME/wallpaper/default.jpg"
 blur="50x30"
 blur=$(cat $blur_file)
 
