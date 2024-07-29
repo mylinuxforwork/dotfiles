959258acef73324dcc7b792e6372e5f1edcdb2a9
Stephan Raabe <mail@stephan-raabe.de>
2024-07-29 15:57:36 +0200

    Updates

--- dotfiles/hypr/scripts/init-wallpaper-engine.sh
+++ dotfiles/hypr/scripts/init-wallpaper-engine.sh
@@ -14,14 +14,14 @@ if [ "$wallpaper_engine" == "swww" ] ;then
     swww init
     swww-daemon --format xrgb
     sleep 0.5
-    ~/dotfiles/hypr/scripts/wallpaper.sh init
+    ~/.config/hypr/scripts/wallpaper.sh init
 elif [ "$wallpaper_engine" == "hyprpaper" ] ;then    
     # hyprpaper
     echo ":: Using hyprpaper"
     sleep 0.5
-    ~/dotfiles/hypr/scripts/wallpaper.sh init
+    ~/.config/hypr/scripts/wallpaper.sh init
 else
     echo ":: Wallpaper Engine disabled"
-    ~/dotfiles/hypr/scripts/wallpaper.sh init
+    ~/.config/hypr/scripts/wallpaper.sh init
 fi
 
