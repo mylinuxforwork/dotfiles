5fa7a842ca992d3bb9f0a7f787efd7ba0f57fd39
Stephan Raabe <mail@stephan-raabe.de>
2024-08-07 12:03:18 +0200

    Updates

--- dotfiles/.config/hypr/scripts/ml4w-welcome-autostart.sh
+++ dotfiles/.config/hypr/scripts/ml4w-welcome-autostart.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-if [ ! -f $HOME/.cache/ml4w-welcome-autostart ] ;then
-    echo ":: Autostart of ML4W Welcome App enabled."
-    if [ -f $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage ] ;then
-        echo ":: Starting ML4W Welcome App ..."
-        sleep 2
-        $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage
-    else
-        echo ":: ML4W Welcome App not found."
-    fi
+if [ ! -f ~/.cache/ml4w-post-install ] ;then
+    if [ ! -f $HOME/.cache/ml4w-welcome-autostart ] ;then
+        echo ":: Autostart of ML4W Welcome App enabled."
+        if [ -f $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage ] ;then
+            echo ":: Starting ML4W Welcome App ..."
+            sleep 2
+            $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage
+        else
+            echo ":: ML4W Welcome App not found."
+        fi
 
+    else
+        echo ":: Autostart of ML4W Welcome App disabled."
+    fi
 else
-    echo ":: Autostart of ML4W Welcome App disabled."
+    terminal=$(cat ~/.config/ml4w/settings/terminal.sh)
+    $terminal --class dotfiles-floating -e ~/.config/ml4w/postinstall.sh
+    rm ~/.cache/ml4w-post-install
+    $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage
 fi
