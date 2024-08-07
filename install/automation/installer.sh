da9d2d3f7054fb0a375af53dc4025c9b5e2d952a
Stephan Raabe <mail@stephan-raabe.de>
2024-08-07 21:34:15 +0200

    Updates

--- install/automation/installer.sh
+++ install/automation/installer.sh
@@ -2,9 +2,6 @@
 # Installation method
 # ------------------------------------------------------
 
-echo -e "${GREEN}"
-figlet "Packages"
-echo -e "${NONE}"
 echo ":: AUTOMATION: New packages will be installed"
 force_install=0
 echo
