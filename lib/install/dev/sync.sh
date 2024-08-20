ea70b9a2f56440e506bddb06fc817ff3625074f8
Stephan Raabe <mail@stephan-raabe.de>
2024-08-20 16:48:21 +0200

    Add key binding for Mission Center

--- lib/install/dev/sync.sh
+++ lib/install/dev/sync.sh
@@ -4,7 +4,7 @@
 if [ ! -z $1 ] ;then
     if [ -d ~/.ml4w-dotfiles/"$1" ] ;then
         echo "Folder exists. Start rsync now ..."
-        rsync -avhp -I --exclude-from=$HOME/.ml4w-dotfiles/$1/install/dev/excludes.txt ~/.ml4w-dotfiles/dotfiles/dotfiles/  ~/dotfiles
+        rsync -avhp -I --exclude-from=$HOME/.ml4w-dotfiles/$1/lib/install/dev/excludes.txt ~/.ml4w-dotfiles/dotfiles/share/dotfiles/  ~/dotfiles
     else 
         echo "Folder ~/.ml4w-dotfiles/$1 not found."
     fi
