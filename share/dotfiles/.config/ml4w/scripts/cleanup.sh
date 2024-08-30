5490cd4018b94fb80a50238d158246770b03caba
Stephan Raabe <mail@stephan-raabe.de>
2024-08-30 23:30:53 +0200

    Update header

--- share/dotfiles/.config/ml4w/scripts/cleanup.sh
+++ share/dotfiles/.config/ml4w/scripts/cleanup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 clear
 aur_helper="$(cat ~/.config/ml4w/settings/aur.sh)"
-cat <<"EOF"
-  ____ _                                
- / ___| | ___  __ _ _ __    _   _ _ __  
-| |   | |/ _ \/ _` | '_ \  | | | | '_ \ 
-| |___| |  __/ (_| | | | | | |_| | |_) |
- \____|_|\___|\__,_|_| |_|  \__,_| .__/ 
-                                 |_|    
-
-EOF
-
+figlet -f smslant "Updates"
+echo
 sudo pacman -Rns $(pacman -Qtdq)
 
 $aur_helper -Scc
