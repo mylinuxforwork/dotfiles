ba4ba1d177f5310a97f5d512fc60cc7accfd6052
Stephan Raabe <mail@stephan-raabe.de>
2024-10-03 21:05:15 +0200

    Change ML4W App Icon location

--- lib/install/dotfiles/apps.sh
+++ lib/install/dotfiles/apps.sh
@@ -11,14 +11,17 @@ fi
 # Downloading the App
 sed -i "s|HOME|${HOME}|g" $HOME/.config/ml4w/apps/ml4w-welcome.desktop
 cp $HOME/.config/ml4w/apps/ml4w-welcome.desktop $HOME/.local/share/applications
+cp $HOME/.config/ml4w/apps/com.ml4w.welcome.png $HOME/.local/share/icons/hicolor/128x128/apps
 echo ":: ML4W Welcome App installed successfully"
 
 sed -i "s|HOME|${HOME}|g" $HOME/.config/ml4w/apps/ml4w-dotfiles-settings.desktop
 cp $HOME/.config/ml4w/apps/ml4w-dotfiles-settings.desktop $HOME/.local/share/applications
+cp $HOME/.config/ml4w/apps/com.ml4w.dotfilessettings.png $HOME/.local/share/icons/hicolor/128x128/apps
 echo ":: ML4W Settings App installed successfully"
 
 sed -i "s|HOME|${HOME}|g" $HOME/.config/ml4w/apps/ml4w-hyprland-settings.desktop
 cp $HOME/.config/ml4w/apps/ml4w-hyprland-settings.desktop $HOME/.local/share/applications
+cp $HOME/.config/ml4w/apps/com.ml4w.hyprland.settings.png $HOME/.local/share/icons/hicolor/128x128/apps
 echo ":: ML4W Hyprland Settings App installed successfully"
 
 echo ":: ML4W Uninstaller App installed successfully"
