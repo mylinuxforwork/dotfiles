d3902279ef1195ba063a00d12db252771a15dcf3
Stephan Raabe <mail@stephan-raabe.de>
2024-10-25 18:59:03 +0200

    Add nwg-dock-hyprland config

---
+++ share/dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -0,0 +1,8 @@
+#    ___           __  
+#   / _ \___  ____/ /__
+#  / // / _ \/ __/  '_/
+# /____/\___/\__/_/\_\ 
+#                      
+
+# Add exec-once = ~/.config/nwg-dock-hyprland/launch.sh to ~/.config/hypr/conf/custom.conf
+nwg-dock-hyprland -i 32 -w 5 -mb 10 -ml 10 -mr 10 -x -c "rofi -show drun"
