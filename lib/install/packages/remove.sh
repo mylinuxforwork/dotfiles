19214107e26bef63e9162eca1f1258d5ec757ee8
Stephan Raabe <mail@stephan-raabe.de>
2024-11-05 17:34:22 +0100

    Add bibata bin package

--- lib/install/packages/remove.sh
+++ lib/install/packages/remove.sh
@@ -4,42 +4,49 @@
 
 # Remove Rofi Calc
 if [[ $(_isInstalledPacman "rofi-calc") == 0 ]]; then
-    sudo pacman --noconfirm -Rns rofi-calc
+    sudo pacman --noconfirm -R rofi-calc
     echo ":: rofi-calc removed"
     echo
 fi
 
 # Remove Rofi
 if [[ $(_isInstalledPacman "rofi") == 0 ]]; then
-    sudo pacman --noconfirm -Rns rofi
+    sudo pacman --noconfirm -R rofi
     echo ":: rofi removed"
     echo
 fi
 
 # Remove Swayidle
 if [[ $(_isInstalledPacman "swayidle") == 0 ]]; then
-    sudo pacman --noconfirm -Rns swayidle
+    sudo pacman --noconfirm -R swayidle
     echo ":: swayidle removed"
     echo
 fi
 
 # Remove Swaylock
 if [[ $(_isInstalledAUR "swaylock-effects-git") == 0 ]]; then
-    $aur_helper --noconfirm -Rns swaylock-effects-git
+    $aur_helper --noconfirm -R swaylock-effects-git
     echo ":: swaylock removed"
+    echo
+fi
+
+# Remove bibata-cursor-theme
+if [[ $(_isInstalledAUR "bibata-cursor-theme") == 0 ]]; then
+    $aur_helper --noconfirm -R bibata-cursor-theme
+    echo ":: bibata-cursor-theme removed"
     echo
 fi
 
 # Remove rofi-lbonn-wayland
 if [[ $(_isInstalledAUR "rofi-lbonn-wayland") == 0 ]]; then
-    $aur_helper --noconfirm -Rns rofi-lbonn-wayland
+    $aur_helper --noconfirm -R rofi-lbonn-wayland
     echo ":: rofi-lbonn-wayland removed"
     echo
 fi
 
 # Remove hypridle-bin
 if [[ $(_isInstalledAUR "hypridle-git") == 0 ]]; then
-    $aur_helper --noconfirm -Rns hypridle-git
+    $aur_helper --noconfirm -R hypridle-git
     if [ -f /usr/lib/debug/usr/bin/hypridle.debug ] ;then
         sudo rm /usr/lib/debug/usr/bin/hypridle.debug
         echo ":: /usr/lib/debug/usr/bin/hypridle.debug removed"
@@ -51,7 +58,7 @@ fi
 
 # Remove hyprlock-bin
 if [[ $(_isInstalledAUR "hyprlock-git") == 0 ]]; then
-    $aur_helper --noconfirm -Rns hyprlock-git
+    $aur_helper --noconfirm -R hyprlock-git
     if [ -f /usr/lib/debug/usr/bin/hyprlock.debug ] ;then
         sudo rm /usr/lib/debug/usr/bin/hyprlock.debug
         echo ":: /usr/lib/debug/usr/bin/hyprlock.debug removed"
