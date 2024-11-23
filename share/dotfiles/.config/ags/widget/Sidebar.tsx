557a63144ba550538f8b5f9f74379f9b9650084d
Al Amin Islam Atif <97211464+atif727@users.noreply.github.com>
2024-11-23 06:10:08 +0600

    Update Sidebar.tsx

--- share/dotfiles/.config/ags/widget/Sidebar.tsx
+++ share/dotfiles/.config/ags/widget/Sidebar.tsx
@@ -3,6 +3,98 @@ import Apps from "gi://AstalApps"
 import Wp from "gi://AstalWp"
 import { Variable, GLib, bind, exec, execAsync } from "astal"
 import { Astal, Gtk, Gdk } from "astal/gtk3"
+import Mpris from "gi://AstalMpris"
+
+// taking length
+function lengthStr(length: number) {
+    const min = Math.floor(length / 60)
+    const sec = Math.floor(length % 60)
+    const sec0 = sec < 10 ? "0" : ""
+    return `${min}:${sec0}${sec}`
+}
+
+// used mrpis
+function MediaPlayer({ player }: { player: Mpris.Player }) {
+    const { START, END } = Gtk.Align
+
+    const title = bind(player, "title").as(t =>
+        t || "Unknown Track")
+
+    const artist = bind(player, "artist").as(a =>
+        a || "Unknown Artist")
+
+    const coverArt = bind(player, "coverArt").as(c =>
+        `background-image: url('${c}')`)
+
+        const validateEntry = (entry: string | null | undefined) => {
+            return entry && Astal.Icon.lookup_icon(entry) ? entry : "audio-x-generic-symbolic";
+        };
+        
+        const playerIcon = bind(player, "entry").as(validateEntry);
+        
+        
+
+    const position = bind(player, "position").as(p => player.length > 0
+        ? p / player.length : 0)
+
+    const playIcon = bind(player, "playbackStatus").as(s =>
+        s === Mpris.PlaybackStatus.PLAYING
+            ? "media-playback-pause-symbolic"
+            : "media-playback-start-symbolic"
+    )
+
+    return <box className="MediaPlayer">
+        <box className="cover-art" css={coverArt} />
+        <box className="hi" vertical>
+            <box className="title">
+                <label truncate hexpand halign={START} label={title} />
+                <icon icon={playerIcon} />
+            </box>
+            <label halign={START} valign={START} vexpand wrap label={artist} />
+            <slider
+                visible={bind(player, "length").as(l => l > 0)}
+                onDragged={({ value }) => player.position = value * player.length}
+                value={position}
+            />
+            <centerbox className="actions">
+                <label
+                    hexpand
+                    className="position"
+                    halign={START}
+                    visible={bind(player, "length").as(l => l > 0)}
+                    label={bind(player, "position").as(lengthStr)}
+                />
+                <box>
+                    <button
+                    css={"padding-right: 15px"}
+                        onClicked={() => player.previous()}
+                        visible={bind(player, "canGoPrevious")}>
+                        <icon icon="media-skip-backward-symbolic" />
+                    </button>
+                    <button
+                        onClicked={() => player.play_pause()}
+                        visible={bind(player, "canControl")}>
+                        <icon icon={playIcon} />
+                    </button>
+                    <button
+                    css={"padding-left: 15px"}
+                        onClicked={() => player.next()}
+                        visible={bind(player, "canGoNext")}>
+                        <icon icon="media-skip-forward-symbolic" />
+                    </button>
+                </box>
+                <label
+                    hexpand
+                    className="length"
+                    halign={END}
+                    visible={bind(player, "length").as(l => l > 0)}
+                    label={bind(player, "length").as(l => l > 0 ? lengthStr(l) : "0:00")}
+                />
+            </centerbox>
+        </box>
+    </box>
+}
+
 
 function AudioSlider() {
     const speaker = Wp.get_default()?.audio.defaultSpeaker!
@@ -59,7 +151,7 @@ function openwaybarthemes() {
 }
 
 export default function Sidebar() {
-
+    const mpris = Mpris.get_default()
     const anchor = Astal.WindowAnchor.TOP
         | Astal.WindowAnchor.RIGHT
 
@@ -71,6 +163,18 @@ export default function Sidebar() {
     anchor={anchor}
     >    
     <box className="sidebar" vertical>
+
+{/* you can just cut the lines 169-176 and paste it according to your need of postion in the sidebar */}
+
+    {bind(mpris, "players").as(arr => {
+    const lastPlayer = arr[arr.length - 1]; // Get the last index of the array (you will get alot of players, the last one has all the metadata of the current playing song)
+    return lastPlayer ? (
+        <box css={"margin-bottom: 20px"} halign="left" vertical>
+            <MediaPlayer player={lastPlayer} />
+        </box>
+    ) : null; // Return null if the array is empty
+})}
+
         <box css="padding-bottom:20px;">
             <box className="group" vertical>
                 <box homogeneous>
@@ -102,6 +206,7 @@ export default function Sidebar() {
             <label css="padding-bottom:10px" label="Microphone"></label>
             <MicrophoneSlider />
         </box>
+        
     </box>
 </window>
 }
