f0ada9303a8daade3225e6758aa4a2f63e1ff545
Stephan Raabe <mail@stephan-raabe.de>
2024-12-12 20:05:01 +0100

    Update Install Script

--- share/dotfiles/.config/ml4w/scripts/installupdates.sh
+++ share/dotfiles/.config/ml4w/scripts/installupdates.sh
@@ -5,6 +5,7 @@
 # /___/_//_/___/\__/\_,_/_/_/  \_,_/ .__/\_,_/\_,_/\__/\__/___/
 #                                 /_/                          
 #
+
 sleep 1
 clear
 install_platform="$(cat ~/.config/ml4w/settings/platform.sh)"
@@ -26,23 +27,33 @@ else
     exit;
 fi
 
+_isInstalled() {
+   package="$1";
+   case $install_platform in
+	arch)
+	 check="$($aur_helper -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+	;;
+	fedora) 
+	 check="$(rpm -qa|grep ${package})"
+	;;
+        *)
+  	;;
+    esac
+
+   if [ -n "${check}" ] ; then
+       echo 0; #'0' means 'true' in Bash
+       return; #true
+   fi;
+echo 1; #'1' means 'false' in Bash
+return; #false
+}
+
 # Check if platform is supported
 case $install_platform in
     arch)
         aur_helper="$(cat ~/.config/ml4w/settings/aur.sh)"
 
-        _isInstalledAUR() {
-            package="$1";
-            check="$($aur_helper -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-            if [ -n "${check}" ] ; then
-                echo 0; #'0' means 'true' in Bash
-                return; #true
-            fi;
-            echo 1; #'1' means 'false' in Bash
-            return; #false
-        }
-
-        if [[ $(_isInstalledAUR "timeshift") == "0" ]] ;then
+        if [[ $(_isInstalled "timeshift") == "0" ]] ;then                                        
             echo
             if gum confirm "DO YOU WANT TO CREATE A SNAPSHOT?" ;then
                 echo
@@ -61,14 +72,15 @@ case $install_platform in
             echo
         fi
 
-        $aur_helper
-
-        if [[ $(_isInstalledAUR "flatpak") == "0" ]] ;then
+        if [[ $(_isInstalled "flatpak") == "0" ]] ;then
             flatpak upgrade
         fi
     ;;
     fedora)
         sudo dnf upgrade
+        if [[ $(_isInstalled "flatpak") == "0" ]] ;then
+            flatpak upgrade
+        fi
     ;;
     *)
         echo ":: ERROR - Platform not supported"
