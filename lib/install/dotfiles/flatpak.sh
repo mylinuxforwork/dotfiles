e15fbc883066d61007a34858efc6101c68b75dbc
Stephan Raabe <mail@stephan-raabe.de>
2025-01-28 21:24:40 +0100

    Updates Apps ans swaync

--- lib/install/dotfiles/flatpak.sh
+++ lib/install/dotfiles/flatpak.sh
@@ -2,3 +2,8 @@
 apps_directory="$1"
 cd $apps_directory
 flatpak --user -y --reinstall install com.ml4w.calendar.flatpak
+echo ":: com.ml4w.calendar.flatpak installed"
+flatpak --user -y --reinstall install com.ml4w.welcome.flatpak
+echo ":: com.ml4w.welcome.flatpak installed"
+flatpak --user -y --reinstall install com.ml4w.settings.flatpak
+echo ":: com.ml4w.setting.flatpak installed"
