1c48fd8fd731ea0e4f962f679723c21cc6c7f751
Stephan Raabe <mail@stephan-raabe.de>
2025-02-02 11:22:02 +0100

    Added 0 updates status to waybar

--- share/dotfiles/.config/ml4w/scripts/updates.sh
+++ share/dotfiles/.config/ml4w/scripts/updates.sh
@@ -53,4 +53,6 @@ fi
 
 if [ "$updates" -gt $threshhold_green ]; then
     printf '{"text": "%s", "alt": "%s", "tooltip": "Click to update your system", "class": "%s"}' "$updates" "$updates" "$css_class"
+else
+    printf '{"text": "0", "alt": "0", "tooltip": "No updates available", "class": "green"}'
 fi
