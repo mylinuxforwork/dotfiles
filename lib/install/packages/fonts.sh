b73d97fd8bc82c894b06bee8981aa0bd7bfb4ffb
Stephan Raabe <mail@stephan-raabe.de>
2025-02-02 11:13:04 +0100

    Updates font installation

--- lib/install/packages/fonts.sh
+++ lib/install/packages/fonts.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Check if the JetBrainsMono folder exists and delete it if it does
+# Check if the JetBrainsMono folder exists
 if [ -d ~/.local/share/fonts/JetBrainsMonoNerd ]; then
-    rm -rf ~/.local/share/fonts/JetBrainsMonoNerd
+    _writeLogTerminal 0 "JetBrainsMono fonts is already installed"
+else
+    # Download JetBrainsMonoNerdFonts
+    DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
+    curl -o $HOME/Downloads/JetBrainsMono.tar.xz -OL "$DOWNLOAD_URL" &>> $(_getLogFile)
+
+    # Install the fonts
+    mkdir -p ~/.local/share/fonts/JetBrainsMonoNerd &>> $(_getLogFile)
+
+    # Extract the new files into the JetBrainsMono folder and log the output
+    tar -xJkf $HOME/Downloads/JetBrainsMono.tar.xz -C ~/.local/share/fonts/JetBrainsMonoNerd
+    _writeLogTerminal 0 "JetBrainsMono fonts installed"
 fi
 
-# Download JetBrainsMonoNerdFonts
-DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
-curl -o $HOME/Downloads/JetBrainsMono.tar.xz -OL "$DOWNLOAD_URL" &>> $(_getLogFile)
-
-# Install the fonts
-mkdir -p ~/.local/share/fonts/JetBrainsMonoNerd &>> $(_getLogFile)
-# Extract the new files into the JetBrainsMono folder and log the output
-tar -xJkf $HOME/Downloads/JetBrainsMono.tar.xz -C ~/.local/share/fonts/JetBrainsMonoNerd
-_writeLogTerminal 0 "JetBrainsMono fonts installed"
-
 # Install FiraCode to enable icons on sequoia theme
-if [ ! -d /usr/share/fonts/FiraCode ]; then
+if [ -d /usr/share/fonts/FiraCode ]; then
+    _writeLogTerminal 0 "FiraCode fonts is already installed"
+else
     sudo cp -rf $fonts_directory/FiraCode /usr/share/fonts
     _writeLogTerminal 0 "FiraCode fonts installed"
 fi
