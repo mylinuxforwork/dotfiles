feaa943c652a0a585ddce0c45989c37bbb45bd31
Stephan Raabe <mail@stephan-raabe.de>
2025-02-05 13:38:00 +0100

    Fix remove legacy apps

--- lib/install/dotfiles/apps.sh
+++ lib/install/dotfiles/apps.sh
@@ -3,6 +3,20 @@
 # ------------------------------------------------------
 _writeHeader "Apps"
 
+_removeLegacyApp() {
+    app_name=$1
+    if [ -f /usr/share/applications/$app_name.desktop ]; then
+        sudo rm /usr/share/applications/$app_name.desktop
+    fi
+    if [ -f /usr/share/icons/hicolor/128x128/apps/$app_name.png ]; then
+        sudo rm /usr/share/icons/hicolor/128x128/apps/$app_name.png
+    fi
+    if [ -f /usr/bin/$app_name ]; then
+        sudo rm /usr/bin/$app_name
+        _writeLog 1 "Legacy $app_name App removed successfully"
+    fi
+}
+
 # Create local applications folder if not exits
 if [ ! -d $HOME/.local/share/applications/ ]; then
     mkdir $HOME/.local/share/applications
@@ -10,21 +24,8 @@ if [ ! -d $HOME/.local/share/applications/ ]; then
 fi
 
 # Remove legacy ML4W Apps
-app_name="com.ml4w.welcome"
-if [ -f /usr/bin/$app_name ]; then
-    sudo rm /usr/share/applications/$app_name.desktop
-    sudo rm $apps_directory/$app_name.png /usr/share/icons/hicolor/128x128/apps/$app_name.png
-    sudo rm /usr/bin/$app_name
-    _writeLog 1 "Legacy ML4W Welcome App removed successfully"
-fi
-
-app_name="com.ml4w.dotfilessettings"
-if [ -f /usr/bin/$app_name ]; then
-    sudo rm /usr/share/applications/$app_name.desktop
-    sudo rm $apps_directory/$app_name.png /usr/share/icons/hicolor/128x128/apps/$app_name.png
-    sudo rm /usr/bin/$app_name
-    _writeLog 1 "Legacy ML4W Settings App removed successfully"
-fi
+_removeLegacyApp "com.ml4w.welcome"
+_removeLegacyApp "com.ml4w.dotfilessettings"
 
 # Install ML4W Hyprland Settings App
 app_name="com.ml4w.hyprland.settings"
