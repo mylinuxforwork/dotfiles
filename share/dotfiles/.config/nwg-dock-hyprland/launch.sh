284b23b72cec421a54fbf884591ec0f83fcf4200
fesowowako <kirohany1001@gmail.com>
2025-02-07 19:00:49 +0200

    Refine Zsh config with keybindings and history improvements
    - Added custom keybindings for easier history navigation (Ctrl+P and Ctrl+N for search, Esc+w for killing text region).
    - Improved history behavior with options to share history across sessions, ignore space-prefixed commands, and remove duplicate entries.
    - Organized Zsh configuration to include additional customizations for improved usability and cleaner command history handling.

--- share/dotfiles/.config/nwg-dock-hyprland/launch.sh
+++ share/dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -12,7 +12,7 @@ if [ ! -f $HOME/.config/ml4w/settings/dock-disabled ]; then
     else
         style="style-dark.css"
     fi
-    nwg-dock-hyprland -i 32 -w 5 -mb 10 -ml 10 -mr 10 -x -s $style -c  "rofi -show drun"
+    nwg-dock-hyprland -i 32 -w 5 -mb 10 -ml 10 -mr 10 -x -s $style -c "rofi -show drun"
 else
     echo ":: Dock disabled"
 fi
