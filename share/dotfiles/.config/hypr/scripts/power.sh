a2824b1ebb0aefaaa692ff8ce5229d2cf43b86a9
Stephan Raabe <mail@stephan-raabe.de>
2025-02-26 15:06:06 +0100

    Update power script

--- share/dotfiles/.config/hypr/scripts/power.sh
+++ share/dotfiles/.config/hypr/scripts/power.sh
@@ -1,56 +1,77 @@
 #!/bin/bash
-#  ____
-# |  _ \ _____      _____ _ __
-# | |_) / _ \ \ /\ / / _ \ '__|
-# |  __/ (_) \ V  V /  __/ |
-# |_|   \___/ \_/\_/ \___|_|
-#
+#    ___                    
+#   / _ \___ _    _____ ____
+#  / ___/ _ \ |/|/ / -_) __/
+# /_/   \___/__,__/\__/_/   
+#                           
 
-# Check if eww is open
-FILE="$HOME/.cache/ml4w_sidebar"
+terminate_clients() {
+  TIMEOUT=60
+  # Get a list of all client PIDs in the current Hyprland session
+  client_pids=$(hyprctl clients -j | jq -r '.[] | .pid')
+
+  # Send SIGTERM (kill -15) to each client PID and wait for termination
+  for pid in $client_pids; do
+    echo ":: Sending SIGTERM to PID $pid"
+    kill -15 $pid
+  done
+
+  start_time=$(date +%s)
+  for pid in $client_pids; do
+    # Wait for the process to terminate
+    while kill -0 $pid 2>/dev/null; do
+      current_time=$(date +%s)
+      elapsed_time=$((current_time - start_time))
+
+      if [ $elapsed_time -ge $TIMEOUT ]; then
+        echo ":: Timeout reached."
+        return 0
+      fi
+
+      echo ":: Waiting for PID $pid to terminate..."
+      sleep 1
+    done
+
+    echo ":: PID $pid has terminated."
+  done
+}
 
 if [[ "$1" == "exit" ]]; then
-    echo ":: Exit"
-    if [[ -f "$FILE" ]]; then
-        rm $FILE
-    fi
-    sleep 0.5
-    hyprctl dispatch exit
-    sleep 2
+  echo ":: Exit"
+  terminate_clients
+  sleep 0.5
+  hyprctl dispatch exit
+  sleep 2
 fi
 
 if [[ "$1" == "lock" ]]; then
-    echo ":: Lock"
-    sleep 0.5
-    hyprlock
+  echo ":: Lock"
+  sleep 0.5
+  hyprlock
 fi
 
 if [[ "$1" == "reboot" ]]; then
-    echo ":: Reboot"
-    if [[ -f "$FILE" ]]; then
-        rm $FILE
-    fi
-    sleep 0.5
-    systemctl reboot
+  echo ":: Reboot"
+  terminate_clients
+  sleep 0.5
+  systemctl reboot
 fi
 
 if [[ "$1" == "shutdown" ]]; then
-    echo ":: Shutdown"
-    if [[ -f "$FILE" ]]; then
-        rm $FILE
-    fi
-    sleep 0.5
-    systemctl poweroff
+  echo ":: Shutdown"
+  terminate_clients
+  sleep 0.5
+  systemctl poweroff
 fi
 
 if [[ "$1" == "suspend" ]]; then
-    echo ":: Suspend"
-    sleep 0.5
-    systemctl suspend
+  echo ":: Suspend"
+  sleep 0.5
+  systemctl suspend
 fi
 
 if [[ "$1" == "hibernate" ]]; then
-    echo ":: Hibernate"
-    sleep 1
-    systemctl hibernate
+  echo ":: Hibernate"
+  sleep 1
+  systemctl hibernate
 fi
