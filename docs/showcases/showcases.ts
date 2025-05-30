a11efe746ae44aa3e0b1d26f472d4c317d4283a4
Hari Chalise <ingoprivate@gmail.com>
2025-05-30 22:52:14 +0545

    fix image path

--- docs/showcases/showcases.ts
+++ docs/showcases/showcases.ts
@@ -8,11 +8,6 @@ const showcases: Showcase[] = [
     image: "/dotfiles/ss/ss.png",
     title: "",
   },
-// using this url because the image url has expired or something in repo README, i can't download this. if stephan provides the image, we can change it later
-  {
-    image: "https://private-user-images.githubusercontent.com/145253254/411322972-2d281632-762f-465c-99e2-6933f1507cac.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDg2MjA4MTcsIm5iZiI6MTc0ODYyMDUxNywicGF0aCI6Ii8xNDUyNTMyNTQvNDExMzIyOTcyLTJkMjgxNjMyLTc2MmYtNDY1Yy05OWUyLTY5MzNmMTUwN2NhYy5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUzMFQxNTU1MTdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNTMzODk4OTExNzNlMDRkZmY1ODU1ZmM3MmM2MjdiYjMwMDI5YjYwOGNmZWU5ZjljMGM0ZjVkM2RlZWMwNmY0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.A-TcJ5Wde2A8gO5Nu0gg-WRtwijzeQzYeOoKJOIL9GI",
-    title: "",
-  },
   {
     image: "/dotfiles/ss/ss1.png",
     title: "",
@@ -35,6 +30,10 @@ const showcases: Showcase[] = [
   },
   {
     image: "/dotfiles/update.png",
+    title: "",
+  },
+  {
+    image: "/dotfiles/screenshot1.png",
     title: "",
   },
   {
