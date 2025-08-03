0ddeea4523f104d23488cd22f9a24fbdecd300e2
Stephan Raabe <mail@stephan-raabe.de>
2025-08-03 16:18:44 +0200

    Updates

--- setup/setup-opensuse.sh
+++ setup/setup-opensuse.sh
@@ -117,17 +117,6 @@ _installPackages "${packages[@]}"
 _installPackages "${hyprland[@]}"
 
 # --------------------------------------------------------------
-# Snap
-# --------------------------------------------------------------
-
-sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed snappy
-sudo zypper --gpg-auto-import-keys refresh
-sudo zypper dup --from snappy
-sudo zypper -n install snapd
-sudo systemctl enable --now snapd
-sudo systemctl enable --now snapd.apparmor
-
-# --------------------------------------------------------------
 # Create .local/bin folder
 # --------------------------------------------------------------
 
