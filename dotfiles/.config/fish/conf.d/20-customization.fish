5265a926c0607a2a59b30372528c8ebfd0580b5e
Stephan Raabe <mail@stephan-raabe.de>
2025-08-24 10:53:17 +0200

    Update fish config

--- dotfiles/.config/fish/conf.d/20-customization.fish
+++ dotfiles/.config/fish/conf.d/20-customization.fish
@@ -5,5 +5,5 @@
 # -----------------------------------------------------
 # Prompt
 # -----------------------------------------------------
-eval "$(oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml)"
-# eval "$(oh-my-posh init fish --config $HOME/.config/ohmyposh/EDM115-newline.omp.json)"
+eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml)"
+# eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/EDM115-newline.omp.json)"
