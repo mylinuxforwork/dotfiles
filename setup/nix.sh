38b6a15771861c38498633285053108ae7bbea28
Stephan Raabe <mail@stephan-raabe.de>
2025-09-03 13:13:24 +0200

    Update

--- setup/nix.sh
+++ setup/nix.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Enable Flakes: First, ensure you have flakes enabled in your Nix configuration. Add experimental-features = nix-command flakes to your /etc/nix/nix.conf or ~/.config/nix/nix.conf.
-# Create Files: Place both flake.nix and home.nix in a new directory, e.g., ~/dotfiles.
-# Replace Username: In flake.nix, change "your-username" to your actual username.
-# Install Dependencies: Run the following command from the same directory where you created the files:
-# nix develop --command sh -c "nix-shell --command 'home-manager switch --flake .'"
-# home-manager switch --flake .#your-username
-
 # /etc/nixos/configuration.nix
+# Add git and flatpak
+
 # Enable the Flakes feature and the accompanying new nix command-line tool
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
+# nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-# Install flake
-nix run home-manager switch --flake .#raabe
+# Rebuild system
+# sudo nixos-rebuild switch
 
-# https://nixos-and-flakes.thiscute.world/
+# Install packages with flake
+# nix profile install /path/to/your/flake/directory
