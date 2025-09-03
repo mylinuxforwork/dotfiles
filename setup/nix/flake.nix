75049b11160d99f0fa8476ef181ccd449e09b29a
Stephan Raabe <mail@stephan-raabe.de>
2025-09-03 14:25:36 +0200

    Updates

--- setup/nix/flake.nix
+++ setup/nix/flake.nix
@@ -1,91 +1,104 @@
-# Execute it with nix profile install /path/to/your/flake/directory
 {
-    description = "The ML4W Dotfiles for Hyprland Packages";
+  description = "The ML4W Dotfiles for Hyprland System-wide Packages";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-    outputs = { self, nixpkgs }:
-        let
-            system = "x86_64-linux";
-            pkgs = nixpkgs.legacyPackages.${system};
-        in 
-        {
-            packages.${system}.default = pkgs.buildEnv {
-            name = "ml4w-dotfiles-environment";
-            paths = with pkgs; [
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+      # This is the key part: A flake-based NixOS configuration
+      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          # This module imports your existing configuration.nix and merges it.
+          # You can safely leave this out if your existing configuration.nix is minimal.
+          # If you want to use this flake as your primary config, you should define
+          # all of your other system settings here.
+          ./configuration.nix
 
-                # General
-                wget
-                curl
-                unzip
-                gum
-                rsync
-                git
-                figlet
-                xdg-utils
-                xdg-user-dirs
-                libnotify
-                vim
-                python3
-                flatpak
-                jq
-                xclip
-                neovim
-                fzf
-                pavucontrol
+          # All your system-wide packages and configurations go here
+          {
+            # Install all of your user-level programs and packages system-wide
+            environment.systemPackages = with pkgs; [
+              # General
+              wget
+              curl
+              unzip
+              gum
+              rsync
+              git
+              figlet
+              xdg-utils
+              xdg-user-dirs
+              libnotify
+              vim
+              python3
+              flatpak
+              jq
+              xclip
+              neovim
+              fzf
+              pavucontrol
 
-                # Hyprland
-                hyprland
-                hyprpaper
-                hyprlock
-                hypridle
-                hyprland-qt-support
-                hyprpicker
-                xdg-desktop-portal-gtk
-                xdg-desktop-portal-hyprland
+              # Hyprland
+              hyprland
+              hyprpaper
+              hyprlock
+              hypridle
+              hyprland-qt-support
+              hyprpicker
+              xdg-desktop-portal-gtk
+              xdg-desktop-portal-hyprland
 
-                # Terminal
-                kitty
+              # Terminal
+              kitty
 
-                # Tools
-                kdePackages.qtwayland
-                fastfetch
-                matugen
-                eza
-                brightnessctl
-                networkmanagerapplet
-                imagemagick
-                htop
-                blueman
-                grim
-                slurp
-                cliphist
-                nwg-look
-                libsForQt5.qt5ct
-                kdePackages.qt6ct
-                waybar
-                rofi-wayland
-                polkit_gnome
-                zsh
-                zsh-completions
-                python3Packages.screeninfo
-                pavucontrol
-                papirus-icon-theme
-                swaynotificationcenter
-                gvfs
-                wlogout
-                waypaper
-                grimblast
-                bibata-cursors
-                nwg-dock-hyprland
-                power-profiles-daemon
-                pywalfox-native
-                vlc
-                nautilus
+              # Tools
+              kdePackages.qtwayland
+              fastfetch
+              matugen
+              eza
+              brightnessctl
+              networkmanagerapplet
+              imagemagick
+              htop
+              blueman
+              grim
+              slurp
+              cliphist
+              nwg-look
+              libsForQt5.qt5ct
+              kdePackages.qt6ct
+              waybar
+              rofi-wayland
+              polkit_gnome
+              zsh
+              zsh-completions
+              python3Packages.screeninfo
+              pavucontrol
+              papirus-icon-theme
+              swaynotificationcenter
+              gvfs
+              wlogout
+              waypaper
+              grimblast
+              bibata-cursors
+              nwg-dock-hyprland
+              power-profiles-daemon
+              pywalfox-native
+              vlc
+              nautilus
+            ];
 
-                # Fonts
+            # This enables fontconfig and installs your fonts system-wide
+            fonts = {
+              fontconfig.enable = true;
+              packages = with pkgs; [
                 noto-fonts
                 noto-fonts-cjk-sans
                 noto-fonts-emoji
@@ -94,8 +107,10 @@
                 fira-sans
                 fira-code
                 fira-code-symbols
-
-            ];
-        };
+              ];
+            };
+          }
+        ];
+      };
     };
 }
