ad147efa8e864fb47304bf9de96c3d7307201671
Stephan Raabe <mail@stephan-raabe.de>
2025-09-24 15:46:25 +0200

    Update

--- dotfiles/.config/ml4w/scripts/installupdates.sh
+++ dotfiles/.config/ml4w/scripts/installupdates.sh
@@ -45,8 +45,9 @@ sleep 1
 clear
 figlet -f smslant "Updates"
 echo
-
-if gum confirm "DO YOU WANT TO START THE UPDATE NOW?"; then
+primarycolor=$(cat ~/.config/ml4w/colors/primary)
+onsurfacecolor=$(cat ~/.config/ml4w/colors/onsurface)
+if gum confirm --selected.background=$primarycolor --prompt.foreground=$onsurfacecolor "DO YOU WANT TO START THE UPDATE NOW?"; then
     echo
     echo ":: Update started..."
 elif [ $? -eq 130 ]; then
