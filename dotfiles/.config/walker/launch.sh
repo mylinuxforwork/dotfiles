8c959973196bc488caba45751ea8621881707703
Stephan Raabe <mail@stephan-raabe.de>
2025-10-19 11:35:17 +0200

    Update walker launch.sh

--- dotfiles/.config/walker/launch.sh
+++ dotfiles/.config/walker/launch.sh
@@ -3,11 +3,12 @@
 # Check if elephant is running
 if pgrep "elephant" > /dev/null
 then
-  # Send this diagnostic message to Standard Error (2>&1)
-  echo ":: Elephant is running. ::" >&2
+    # Send this diagnostic message to Standard Error (2>&1)
+    echo ":: Elephant is running. ::" >&2
 else
-  # Send this diagnostic message to Standard Error (2>&1)
-  echo ":: Elephant is NOT running. ::" >&2
+    # Send this diagnostic message to Standard Error (2>&1)
+    echo ":: Elephant is NOT running. ::" >&2
+    elephant &
 fi
 
 # Send this diagnostic message to Standard Error (2>&1)
