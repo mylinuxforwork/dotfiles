c5bb11efabdd5c25e1c0243acbd18f3d2e7dce02
Stephan Raabe <mail@stephan-raabe.de>
2025-11-12 18:17:37 +0100

    Add desktop themes

---
+++ dotfiles/.config/ml4w/themes/modern/theme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# ML4W Theme Modern
+
+# Set waybar
+echo "/ml4w-modern;/ml4w-modern/default" > $HOME/.config/ml4w/settings/waybar-theme.sh
+$HOME/.config/waybar/launch.sh &
+
+# Set nwg-dock-hyprland
+echo "modern" > $HOME/.config/ml4w/settings/dock-theme
+$HOME/.config/nwg-dock-hyprland/launch.sh &
+
+# Set swaync
+echo '@import "themes/modern/style.css";' > $HOME/.config/swaync/style.css
+swaync-client -rs
+
+# Set wlogout
+echo '@import "themes/modern/style.css";' > $HOME/.config/wlogout/style.css
+
+# Set rofi
+echo '* { border-width: 2px; }' > $HOME/.config/ml4w/settings/rofi-border.rasi
+
+echo ":: Theme set to Modern"
