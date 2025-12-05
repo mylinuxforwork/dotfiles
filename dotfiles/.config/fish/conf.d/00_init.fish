37c129779bde63b0594d22bfd42c0d406d5eb535
the-iron-ryan <ryandougherty25@gmail.com>
2025-12-05 02:27:29 -0800

    Override fish with ssh agent

--- dotfiles/.config/fish/conf.d/00_init.fish
+++ dotfiles/.config/fish/conf.d/00_init.fish
@@ -12,3 +12,14 @@ export EDITOR=nvim
 set -U fish_user_paths "/usr/lib/ccache/bin/"
 set -U fish_user_paths "$fish_user_paths" "~/.cargo/bin/"
 set -U fish_user_paths "$fish_user_paths" "~/.local/bin/"
+
+# -----------------------------------------------------
+# Setup
+# -----------------------------------------------------
+# Start up ssh-agent
+if status is-interactive
+    # Only start an agent if we don't already have one
+    if not set -q SSH_AUTH_SOCK
+        eval (ssh-agent -c) >/dev/null
+    end
+end
