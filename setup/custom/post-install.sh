6ee95aac63aee9cc0629a17f26669955830d55f1
corazon <99136079+corazon008@users.noreply.github.com>
2025-12-10 20:18:16 +0100

    forget to copy theme

--- setup/custom/post-install.sh
+++ setup/custom/post-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# link dotfiles 
+# link dotfiles
 if [ -f custom/link-dotfiles.sh ]; then
     ./custom/link-dotfiles.sh
 else
@@ -17,6 +17,14 @@ Session=hyprland
 [Theme]
 Current=sugar-candy
 """ | sudo tee /etc/sddm.conf > /dev/null
+
+# Copy sugar-candy theme
+if [ -d custom/sugar-candy ]; then
+    sudo cp -r custom/sugar-candy /usr/share/sddm/themes/
+else
+  sudo cp -r sugar-candy /usr/share/sddm/themes/
+fi
+
 
 # Create empty wireguard config if not exists
 sudo touch /etc/wireguard/laptop.conf
