87ff9f1a2ea2642ead2dcae1b29194cffbb5a378
Stephan Raabe <mail@stephan-raabe.de>
2026-01-14 10:22:10 +0100

    Fix Fedora Copr for swww

--- setup/setup-fedora.sh
+++ setup/setup-fedora.sh
@@ -96,7 +96,6 @@ sudo dnf copr remove --assumeyes solopasha/hyprland
 sudo dnf copr enable --assumeyes sdegler/hyprland
 sudo dnf copr enable --assumeyes peterwu/rendezvous
 sudo dnf copr enable --assumeyes wef/cliphist
-sudo dnf copr enable --assumeyes wef/swww
 sudo dnf copr enable --assumeyes tofik/nwg-shell
 sudo dnf copr enable --assumeyes che/nerd-fonts
 sudo dnf copr enable --assumeyes erikreider/SwayNotificationCenter
