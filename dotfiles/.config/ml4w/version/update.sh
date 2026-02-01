9151418ebc6f949729dd3f7d8b1bad504f0a501e
Stephan Raabe <mail@stephan-raabe.de>
2026-02-01 21:29:41 +0100

    Update Version

--- dotfiles/.config/ml4w/version/update.sh
+++ dotfiles/.config/ml4w/version/update.sh
@@ -3,23 +3,62 @@
 # Check for updates
 # ------------------------------------------------------
 
-source ~/.config/ml4w/version/library.sh
+vercomp() {
+    if [[ $1 == $2 ]]; then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
+        ver1[i]=0
+    done
+    for ((i = 0; i < ${#ver1[@]}; i++)); do
+        if ((10#${ver1[i]:=0} > 10#${ver2[i]:=0})); then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            return 2
+        fi
+    done
+    return 0
+}
+
+testvercomp() {
+    vercomp $1 $2
+    case $? in
+        0) op='=' ;;
+        1) op='>' ;;
+        2) op='<' ;;
+    esac
+    if [[ $op != $3 ]]; then
+        # No update available
+        echo "1"
+    else
+        # Update available
+        echo "0"
+    fi
+}
 
 # Get latest tag from GitHub
 get_latest_release() {
-    v_online=$(curl --silent "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=ml4w-hyprland")
-    v_full_online=$(grep -m 1 'pkgver' <<<$v_online | sed 's/^$/pkgver/')
-    echo ${v_full_online/pkgver=/}
+    local repo="${1:-mylinuxforwork/dotfiles}" # Defaults to the CORRECT repo
+    local url="https://api.github.com/repos/$repo/releases/latest"
+    
+    # Fetch data
+    local response=$(curl -s "$url")
+    
+    # Extract tag (e.g., "v2.9.8")
+    local tag=$(echo "$response" | jq -r .tag_name)
+
+    # Validate
+    if [ "$tag" == "null" ] || [ -z "$tag" ]; then
+        return 1
+    fi
+    echo "$tag"
 }
 
-# Check for internet connection
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-
-    version=$(cat ~/.config/ml4w/version/name)
-    online=$(get_latest_release "mylinuxforwork/hyprland-dotfiles")
-    echo $version "<" $online
-    testvercomp $version $online "<"
-else
-    # Network is down
-    echo "1"
-fi
+REMOTE_TAG=$(get_latest_release "mylinuxforwork/dotfiles")
+version=$(cat ~/.config/ml4w/version/name)
+# testvercomp $version $REMOTE_TAG "<"
+testvercomp $version $REMOTE_TAG "<"
