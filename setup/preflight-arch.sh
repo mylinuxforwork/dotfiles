bcffc190f0b0b1ab2e555baea30f612456492a6b
Stephan Raabe <mail@stephan-raabe.de>
2026-02-16 16:36:54 +0100

    Updates

--- setup/preflight-arch.sh
+++ setup/preflight-arch.sh
@@ -5,6 +5,16 @@ yay_installed="false"
 paru_installed="false"
 aur_helper=""
 
+_checkCommandExists() {
+    cmd="$1"
+    if ! command -v "$cmd" >/dev/null; then
+        echo 1
+        return
+    fi
+    echo 0
+    return
+}
+
 _isInstalled() {
     package="$1"
     check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
