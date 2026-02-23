e21247843d0f4d91ddde4838de7c6ca0bd3ea4ce
corazon <99136079+corazon008@users.noreply.github.com>
2026-02-23 11:26:06 +0100

    clean unused files and packages

--- setup/_ml4w-apps.sh
+++ setup/_ml4w-apps.sh
@@ -22,11 +22,6 @@ ml4w_app_repo="dotfiles-sidebar"
 echo ":: Installing $ml4w_app"
 bash -c "$(curl -s https://raw.githubusercontent.com/mylinuxforwork/$ml4w_app_repo/master/setup.sh)"
 
-ml4w_app="com.ml4w.calendar"
-ml4w_app_repo="dotfiles-calendar"
-echo ":: Installing $ml4w_app"
-bash -c "$(curl -s https://raw.githubusercontent.com/mylinuxforwork/$ml4w_app_repo/master/setup.sh)"
-
 ml4w_app="com.ml4w.hyprlandsettings"
 ml4w_app_repo="hyprland-settings"
 echo ":: Installing $ml4w_app"
