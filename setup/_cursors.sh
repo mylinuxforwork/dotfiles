f0d039081b01f37ca72447f41e10007ab9255a56
Stephan Raabe <mail@stephan-raabe.de>
2026-03-02 13:14:52 +0100

    Arc cursors removed.

--- setup/_cursors.sh
+++ setup/_cursors.sh
@@ -40,23 +40,3 @@ tar -xf $download_folder/Bibata-Modern-Ice.tar.xz -C ~/.local/share/icons/
 if [ -d $download_folder ]; then
     rm -rf $download_folder
 fi
-
-# --------------------------------------------------------------
-# Arc Cursors
-# --------------------------------------------------------------
-
-if [ -d ~/.local/share/icons/ArcAurora-cursors ]; then
-    rm -rf ~/.local/share/icons/ArcAurora-cursors
-fi
-
-if [ -d ~/.local/share/icons/ArcDusk-cursors ]; then
-    rm -rf ~/.local/share/icons/ArcDusk-cursors
-fi
-
-if [ -d ~/.local/share/icons/ArcStarry-cursors ]; then
-    rm -rf ~/.local/share/icons/ArcStarry-cursors
-fi
-
-unzip $SCRIPT_DIR/cursors/ArcAurora-cursors.zip -d ~/.local/share/icons/
-unzip $SCRIPT_DIR/cursors/ArcDusk-cursors.zip -d ~/.local/share/icons/
-unzip $SCRIPT_DIR/cursors/ArcStarry-cursors.zip -d ~/.local/share/icons/
