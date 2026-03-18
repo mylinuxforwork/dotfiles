3de8439898a9357b7101933b4126470d08cd8a16
Ava <createchivity@gmail.com>
2026-03-18 16:35:22 +0000

    Revamped the Glass theme Waybar and Dock - GLASS Overhaul/Aero

--- dotfiles/.config/nwg-dock-hyprland/launch.sh
+++ dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
-#    ___           __
+#    ___            __
 #   / _ \___  ____/ /__
 #  / // / _ \/ __/  '_/
 # /____/\___/\__/_/\_\
 #
 
-DOCK_THEME="modern"
+DOCK_THEME="modern"what 
 if [ -f $HOME/.config/ml4w/settings/dock-theme ]; then
     DOCK_THEME=$(cat $HOME/.config/ml4w/settings/dock-theme)
 fi
+
 echo ":: Using Dock Theme $DOCK_THEME"
-echo ":: Dock Autohide $DOCK_AUTOHIDE"
+
 if [ ! -f $HOME/.config/ml4w/settings/dock-disabled ]; then
     killall nwg-dock-hyprland
     sleep 0.5
+
+    # -f: Force full width to reach edges
+    # -mb 10: Bottom margin
+    # -ml 10: Left margin
+    # -mr 10: Right margin
+    # -w 20: High icon wrap; count to ensure it stays a single long bar
+    # -c: Launcher script path
+
     if [ -f $HOME/.config/ml4w/settings/dock-autohide ]; then
-        nwg-dock-hyprland -d -i 32 -w 5 -mb 10 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
+        nwg-dock-hyprland -d -i 32 -f -mb 10 -ml 10 -mr 10 -w 20 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
     else
-        nwg-dock-hyprland -i 32 -w 5 -mb 10 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
+        nwg-dock-hyprland -i 32 -f -mb 10 -ml 10 -mr 10 -w 20 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
     fi
 else
     echo ":: Dock disabled"
