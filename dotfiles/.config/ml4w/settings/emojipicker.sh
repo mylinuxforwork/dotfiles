bd2a0d774ed4dad7f625790fc3cc571498092dbe
Stephan Raabe <mail@stephan-raabe.de>
2026-03-22 16:38:03 +0100

    Updates

--- dotfiles/.config/ml4w/settings/emojipicker.sh
+++ dotfiles/.config/ml4w/settings/emojipicker.sh
@@ -1,1 +1,1 @@
-flatpak run com.ml4w.sidebar
+flatpak run com.tomjwatson.Emote
--- dotfiles/.config/ml4w/settings/installupdates.sh
+++ dotfiles/.config/ml4w/settings/installupdates.sh
@@ -1,1 +1,7 @@
-$(cat ~/.config/ml4w/settings/terminal.sh) --class dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
+#!/usr/bin/env bash
+TERMINAL=$(cat ~/.config/ml4w/settings/terminal.sh)
+if [ $TERMINAL == "ghostty" ]; then
+    ghostty --class=dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
+else
+    $(cat ~/.config/ml4w/settings/terminal.sh) --class dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
+fi
