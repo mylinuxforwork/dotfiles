1a306c079360c8b566eec03ccd6f74290bf11854
Stephan Raabe <mail@stephan-raabe.de>
2026-03-22 17:14:52 +0100

    Updates

--- dotfiles/.config/ml4w/settings/installupdates.sh
+++ dotfiles/.config/ml4w/settings/installupdates.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-TERMINAL=$(cat ~/.config/ml4w/settings/terminal.sh)
-if [ $TERMINAL == "ghostty" ]; then
-    ghostty --class=dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
-else
-    $(cat ~/.config/ml4w/settings/terminal.sh) --class dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
-fi
+$(cat ~/.config/ml4w/settings/terminal.sh) --class dotfiles-floating -e ~/.config/ml4w/scripts/ml4w-install-system-updates
