494fbc67896756e5f29fb94181bffe8b999c552d
Stephan Raabe <mail@stephan-raabe.de>
2026-03-26 11:13:18 +0100

    Updates

--- setup/migrate.sh
+++ setup/migrate.sh
@@ -35,12 +35,24 @@ fi
 
 # openSuse: Set wallpaper engine to hyprpaper
 if command -v zypper &> /dev/null; then 
-    WAYPAPER_CONFIG="$HOME/.config/waypaper/config.ini"
-    if [ -f "$WAYPAPER_CONFIG" ]; then
-        echo ":: Updating waypaper config..."
-        sed -i 's/awww/hyprpaper/g' "$WAYPAPER_CONFIG"
-        echo ":: Successfully switched from awww to hyprpaper."
-    else
-        echo ":: Warning: waypaper config not found at $WAYPAPER_CONFIG. Skipping..."
+    if [ -f $HOME/.mydotfiles/com.ml4w.dotfiles]; then
+        WAYPAPER_CONFIG="$HOME/.mydotfiles/com.ml4w.dotfiles/.config/waypaper/config.ini"
+        if [ -f "$WAYPAPER_CONFIG" ]; then
+            echo ":: Updating waypaper config..."
+            sed -i 's/awww/hyprpaper/g' "$WAYPAPER_CONFIG"
+            echo ":: Successfully switched from awww to hyprpaper."
+        else
+            echo ":: Warning: waypaper config not found at $WAYPAPER_CONFIG. Skipping..."
+        fi
+    fi
+    if [ -f $HOME/.mydotfiles/com.ml4w.dotfiles.stable]; then
+        WAYPAPER_CONFIG="$HOME/.mydotfiles/com.ml4w.dotfiles.stable/.config/waypaper/config.ini"
+        if [ -f "$WAYPAPER_CONFIG" ]; then
+            echo ":: Updating waypaper config..."
+            sed -i 's/awww/hyprpaper/g' "$WAYPAPER_CONFIG"
+            echo ":: Successfully switched from awww to hyprpaper."
+        else
+            echo ":: Warning: waypaper config not found at $WAYPAPER_CONFIG. Skipping..."
+        fi
     fi
 fi
