36ad72cf6cfbf988abe2f092617feb5e2e57a5a9
Stephan Raabe <mail@stephan-raabe.de>
2026-05-12 21:02:06 +0200

    Updates

---
+++ dotfiles/.config/hypr/conf/decorations/gamemode.lua
@@ -0,0 +1,16 @@
+hl.config({
+    decoration = {
+        rounding = 0,
+        active_opacity = 1.0,
+        inactive_opacity = 0.9,
+        fullscreen_opacity = 1.0,
+
+        shadow = {
+            enabled = false
+        },
+
+        blur = {
+            enabled   = false
+        },
+    },
+})
---
+++ dotfiles/.config/hypr/conf/decorations/no-blur.lua
@@ -0,0 +1,20 @@
+hl.config({
+    decoration = {
+        rounding = 10,
+        active_opacity = 1.0,
+        inactive_opacity = 0.9,
+        fullscreen_opacity = 1.0,
+        rounding_power = 2,
+
+        shadow = {
+            enabled = true,
+            range = 32,
+            render_power = 2,
+            color = "rgba(00000050)",
+        },
+
+        blur = {
+            enabled   = false
+        },
+    },
+})
