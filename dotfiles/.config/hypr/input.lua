625393dcf309e4fba14af619e382c680d30aac5e
corazon <99136079+corazon008@users.noreply.github.com>
2026-06-12 19:53:06 +0200

    add options to input

--- dotfiles/.config/hypr/input.lua
+++ dotfiles/.config/hypr/input.lua
@@ -4,17 +4,17 @@
 
 hl.config({
     input = {
-        kb_layout  = "us",
-        kb_variant = "",
-        kb_model   = "",
-        kb_options = "",
-        kb_rules   = "",
+        kb_layout    = "us",
+        kb_variant   = "",
+        kb_model     = "",
+        kb_options   = "grp:win_space_toggle",
+        kb_rules     = "",
 
         follow_mouse = 1,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+        sensitivity  = 0, -- -1.0 - 1.0, 0 means no modification.
 
-        touchpad = {
+        touchpad     = {
             natural_scroll = false,
         },
     },
