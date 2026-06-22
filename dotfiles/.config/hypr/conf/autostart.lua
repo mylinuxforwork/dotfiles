388c719d9c94adb529f7bed68e35e77f733dbbc3
Stephan Raabe <mail@stephan-raabe.de>
2026-06-22 08:09:49 +0200

    Updates

--- dotfiles/.config/hypr/conf/autostart.lua
+++ dotfiles/.config/hypr/conf/autostart.lua
@@ -1,29 +1,41 @@
 hl.on("hyprland.start", function () 
     -- Export variables to systemd
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+
     -- Restart portals so they catch the environment
     hl.exec_cmd("systemctl --user stop xdg-desktop-portal xdg-desktop-portal-hyprland")
     hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland xdg-desktop-portal")
+
     -- Load cursor
     hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 24")
+
     -- Start listeners
     hl.exec_cmd("~/.config/ml4w/listeners.sh --startall")
+
     -- Start polkit daemon
     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+
     -- Restore wallpaper
     hl.exec_cmd("~/.config/ml4w/scripts/ml4w-wallpaper-app --restore")
+
     -- Environment for xdg-desktop-portal-hyprland
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+
     -- Autostart scripts
-    hl.exec_cmd("~/.config/ml4w/scripts/ml4w-autostart")
+    hl.exec_cmd("~/.config/ml4w/scripts/ml4w-autostart > ~/.mydotfiles/ml4w-autostart.log 2>&1")
+
     -- Load GTK settings
     hl.exec_cmd("~/.config/hypr/scripts/gtk.sh")
+
     -- Start swaync
     hl.exec_cmd("swaync")
+
     -- Start hypridle
     hl.exec_cmd("hypridle")
+
     -- Load cliphist history
     hl.exec_cmd("wl-paste --watch cliphist store")
+
     -- Start autostart cleanup
     hl.exec_cmd("~/.config/hypr/scripts/cleanup.sh")
 end)
