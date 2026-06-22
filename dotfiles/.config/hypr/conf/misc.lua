d22fab45ad0c154b7f1f227f38b9d2653a6a1e0e
Stephan Raabe <mail@stephan-raabe.de>
2026-06-22 13:35:57 +0200

    Updates

--- dotfiles/.config/hypr/conf/misc.lua
+++ dotfiles/.config/hypr/conf/misc.lua
@@ -1,6 +1,7 @@
 hl.config({
     misc = {
-        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+        disable_hyprland_logo   = true,
+        force_default_wallpaper = 0,
         disable_splash_rendering = true,
         initial_workspace_tracking = 1,
         on_focus_under_fullscreen = 1,
