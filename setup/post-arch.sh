89278125310a3fb4f198540ab00ed83c90f9f075
Stephan Raabe <mail@stephan-raabe.de>
2026-08-31 15:38:05 +0200

    Updates

--- setup/post-arch.sh
+++ setup/post-arch.sh
@@ -9,13 +9,13 @@ curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 # ML4W Settings App
 # --------------------------------------------------------------
 
-curl -sSL https://raw.githubusercontent.com/mylinuxforwork/ml4w-dotfiles-settings/main/setup.sh | bash
+bash <(curl -s https://raw.githubusercontent.com/mylinuxforwork/ml4w-dotfiles-settings/main/setup.sh)
 
 # --------------------------------------------------------------
 # Quickshell Overview
 # --------------------------------------------------------------
 
-curl -sSL https://raw.githubusercontent.com/mylinuxforwork/ml4w-quickshell-overview/main/install.sh | bash
+bash <(curl -s https://raw.githubusercontent.com/mylinuxforwork/ml4w-quickshell-overview/main/install.sh)
 
 # --------------------------------------------------------------
 # Pipx
